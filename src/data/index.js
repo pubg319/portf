012900423fcf99c7936f960b5327d087fbe120f2
B X Gate's <75904878+pubg319@users.noreply.github.com>
2024-12-07 19:54:51 +0530

    Initial commit

---
+++ src/data/index.js
@@ -0,0 +1,84 @@
+import {
+  algorithms,
+  devnotes,
+  oscs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "hero",
+    title: "X",
+  },
+  {
+    id: "portfolio",
+    title: "Portfolio",
+  },
+  {
+    id: "experience",
+    title: "Experience",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const experiences = [
+  {
+    title: "",
+    company_name: "",
+    date: "",
+    details: [
+      "",
+      "",
+      "",
+    ],
+  },
+  {
+    title: "",
+    company_name: "",
+    date: "",
+    details: [
+      "",
+    ],
+  },
+  {
+    title: "",
+    company_name: "",
+    date: "",
+    details: [
+      ,
+    ],
+  },
+  {
+    title: "",
+    company_name: "",
+    date: "",
+    details: [
+    ],
+  },
+];
+
+const portfolio = [
+  {
+    name: "Open Source Repo",
+    description:
+      "A GitHub repo I know its not good but.....",
+    image: oscs,
+  },
+  {
+    name: "Leet code",
+    description:
+      "Im noob soo..",
+    image: devnotes,
+  },
+  {
+    name: "Cyber Sec",
+    description:
+      "A beginner",
+    image: algorithms,
+  },
+];
+
+export { experiences, portfolio };
+
